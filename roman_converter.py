abc6e3e8d1b5261e7d4faab965270f6a41ac7880
Gabriel Fonseca <gabrielsqsf@gmail.com>
2020-11-21 16:27:50 -0300

    implemented convert logic

--- roman_converter.py
+++ roman_converter.py
@@ -5,4 +5,16 @@ class Converter:
         self.components = roman_components.components
     
     def convert(self, roman_number):
-        return 0
+        limit = len(roman_number)
+        result = 0
+        for i in range(limit):
+            key = roman_number[i]
+            next_key = None
+            result += self.components[key].value
+            if i < limit:
+                next_key = roman_number[i+1]
+            if self.components[key].value < self.components[next_key].value:
+                result -= self.components[next_key].value
+                i += 1
+            
+        return result
