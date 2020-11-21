1da035a4fe80ff3cb5504a09f5df436cc08169af
Gabriel Fonseca <gabrielsqsf@gmail.com>
2020-11-21 16:30:15 -0300

    correction on components

--- roman_components.py
+++ roman_components.py
@@ -1,6 +1,6 @@
 components = {
-    "I": {"value": 1, "previous": None, "next": "V"}
-    "V": {"value": 5, "previous": "I", "next": "X"}
-    "X": {"value": 10, "previous": "V", "next": "L"}
+    "I": {"value": 1, "previous": None, "next": "V"},
+    "V": {"value": 5, "previous": "I", "next": "X"},
+    "X": {"value": 10, "previous": "V", "next": "L"},
     "L": {"value": 50, "previous": "X", "next": None}
 }
